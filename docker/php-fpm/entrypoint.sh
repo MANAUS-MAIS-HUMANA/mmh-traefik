422f5846e95feac6d7c2a8a9ce686f95cb5b28c8
dsouza <sys23@grupopmz.com.br>
2020-04-10 22:10:11 -0400

    Correção do Readme e do entrypoint

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -5,6 +5,9 @@ if [[ ! -d ./vendor ]]; then
     php artisan package:discover --ansi
     php -r "file_exists('.env') || copy('.env.example', '.env');"
     php artisan key:generate --ansi
+
+    chown -R www-data:www-data /var/www
+    chmod -R 755 /var/www/storage
 fi
 
 php-fpm
