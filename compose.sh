669d3ea7037174a707ed392d5a82865a4c5d21a3
dsouza <sys23@grupopmz.com.br>
2020-04-10 21:44:12 -0400

    Clone front e back

--- compose.sh
+++ compose.sh
@@ -9,35 +9,35 @@
 
 echo "Running docker-compose $1..."
 
-DIR=./front
-
-if [[ "$(ls -A ${DIR})" ]]; then
-
-    FILE=./front/Dockerfile
-
-    if [[ ! -f "$FILE" ]]; then
-        yes | cp ./docker/node/Dockerfile ./front/
-    fi
-
-    case "$2" in
-        dev|"")
-            yes | cp .env-dev .env
-            yes | cp ./docker/nginx/back/default-dev.conf ./docker/nginx/back/default.conf
-
-            rm -f acme.json
-
-            sudo docker-compose $1 --remove-orphans
-        ;;
-        prod)
-            yes | cp .env-prod .env
-            yes | cp ./docker/nginx/back/default-prod.conf ./docker/nginx/back/default.conf
-
-            touch acme.json
-            sudo chmod 600 acme.json
-
-            sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml $1 --remove-orphans
-    esac
-
-else
-    echo "A pasta 'front' está vazia, antes de continuar, crie primeiro o projeto."
+if [[ ! -d ./front ]]; then
+    git clone https://github.com/MANAUS-MAIS-HUMANA/mmh-web.git front
 fi
+
+if [[ ! -d ./back ]]; then
+    git clone https://github.com/MANAUS-MAIS-HUMANA/mmh-service.git back
+fi
+
+FILE=./front/Dockerfile
+
+if [[ ! -f "$FILE" ]]; then
+    yes | cp ./docker/node/Dockerfile ./front/
+fi
+
+case "$2" in
+    dev|"")
+        yes | cp .env-dev .env
+        yes | cp ./docker/nginx/back/default-dev.conf ./docker/nginx/back/default.conf
+
+        rm -f acme.json
+
+        sudo docker-compose $1 --remove-orphans
+    ;;
+    prod)
+        yes | cp .env-prod .env
+        yes | cp ./docker/nginx/back/default-prod.conf ./docker/nginx/back/default.conf
+
+        touch acme.json
+        sudo chmod 600 acme.json
+
+        sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml $1 --remove-orphans
+esac
