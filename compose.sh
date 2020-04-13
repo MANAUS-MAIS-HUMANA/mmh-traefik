b774f99980b554f8bffd56de2aaddc3daad437e2
dsouza <sys23@grupopmz.com.br>
2020-04-13 10:47:24 -0400

    Atualização do .env-example, compose.sh e docker-compose

--- compose.sh
+++ compose.sh
@@ -21,8 +21,11 @@ fi
 if [[ ! -d ./back ]]; then
     git clone https://github.com/MANAUS-MAIS-HUMANA/mmh-service.git back
 
+    yes | cp ./docker/php-fpm/Dockerfile ./back/
+    yes | cp ./docker/php-fpm/entrypoint.sh ./back/
+
     echo "Running permissions in back"
-    cd back && sudo chmod -R 755 storage && sudo chmod -R 755 bootstrap/cache/ && cd ..
+    cd back && chmod -R 755 storage && chmod -R 755 bootstrap/cache/ && cd ..
 fi
 
 case "$2" in
@@ -32,14 +35,14 @@ case "$2" in
 
         rm -f acme.json
 
-        sudo docker-compose $1 --remove-orphans
+        docker-compose $1 --remove-orphans
     ;;
     prod)
         yes | cp .env-prod .env
         yes | cp ./docker/nginx/back/default-prod.conf ./docker/nginx/back/default.conf
 
         touch acme.json
-        sudo chmod 600 acme.json
+        chmod 600 acme.json
 
-        sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml $1 --remove-orphans
+        docker-compose -f docker-compose.yml -f docker-compose.prod.yml $1 --remove-orphans
 esac
